33d168427cd3aa49902cf280286b72148f1ed1bb
ayoubrezzouk <rezzouk.ayouble@gmail.com>
2025-07-14 11:25:48 +0200

    feat(topic): publish OrderCreatedEvent
    Steps :
    - create OrderCreatedEvent Object in core package
    - add core package to trusted packages for deserialization
    - configure a topic name in application.properties
    - create topic in the KafkaConfig with 3 brokers and 3 partitions
    - send OrderCreatedEvent in OrderServiceImpl

--- orders-service/src/main/java/com/appsdeveloperblog/orders/config/KafkaConfig.java
+++ orders-service/src/main/java/com/appsdeveloperblog/orders/config/KafkaConfig.java
@@ -1,16 +1,35 @@
 package com.appsdeveloperblog.orders.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaConfig {
 
+    // Step 9 : Create Topic
+    @Value("${orders.events.topic.name}")
+    private String ordersEventsTopicName;
+
+    private final static Integer TOPIC_REPLICATION_FACTOR = 3;
+    private final static Integer TOPIC_NUM_PARTITIONS = 3;
+
+    // Step 4 : check kafkaTemplate
     @Bean
     KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    NewTopic createOrdersEventsTopic() {
+        return TopicBuilder.name(ordersEventsTopicName)
+                .partitions(TOPIC_NUM_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
     }
 
 }
--- orders-service/src/main/java/com/appsdeveloperblog/orders/service/OrderServiceImpl.java
+++ orders-service/src/main/java/com/appsdeveloperblog/orders/service/OrderServiceImpl.java
@@ -1,17 +1,29 @@
 package com.appsdeveloperblog.orders.service;
 
 import com.appsdeveloperblog.core.dto.Order;
+import com.appsdeveloperblog.core.dto.events.OrderCreatedEvent;
 import com.appsdeveloperblog.core.types.OrderStatus;
 import com.appsdeveloperblog.orders.dao.jpa.entity.OrderEntity;
 import com.appsdeveloperblog.orders.dao.jpa.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
+    // Step 5 : Inject KafkaTemplate
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    // Step 8 : Inject OrdersEventTopicName value
+    private final String ordersEventsTopicName;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository,
+                            KafkaTemplate<String, Object> kafkaTemplate,
+                            @Value("${orders.events.topic.name}") String ordersEventsTopicName
+    ) {
         this.orderRepository = orderRepository;
+        this.kafkaTemplate = kafkaTemplate;
+        this.ordersEventsTopicName = ordersEventsTopicName;
     }
 
     @Override
@@ -22,6 +34,17 @@ public class OrderServiceImpl implements OrderService {
         entity.setProductQuantity(order.getProductQuantity());
         entity.setStatus(OrderStatus.CREATED);
         orderRepository.save(entity);
+
+        // Step 3 : Instancing an OrderCreatedEvent when placeOrder
+        OrderCreatedEvent placeOrderCreatedEvent = new OrderCreatedEvent(
+                entity.getId(),
+                entity.getCustomerId(),
+                entity.getProductId(),
+                entity.getProductQuantity()
+        );
+
+        // Step 6 : Send KafkaEvent to Topic
+        kafkaTemplate.send(ordersEventsTopicName, placeOrderCreatedEvent);
 
         return new Order(
                 entity.getId(),

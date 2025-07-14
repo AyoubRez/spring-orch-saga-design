08cd7efc6a9afca7b99a0be8f9e5606fd6fc7c6c
ayoubrezzouk <rezzouk.ayouble@gmail.com>
2025-07-14 16:31:49 +0200

    feat(handle): handle PaymentsCommandsEvent
    Steps :
    - create PaymentsCommandsHandler
    - add @Component and @KafkaListener to  topic payments commands
    - add handler method that creats Payment object and passe it to the paymentService process method

---
+++ payments-service/src/main/java/com/appsdeveloperblog/payments/service/handler/PaymentsCommandsHandler.java
@@ -0,0 +1,41 @@
+package com.appsdeveloperblog.payments.service.handler;
+
+import com.appsdeveloperblog.core.dto.Payment;
+import com.appsdeveloperblog.core.dto.commands.ProcessPaymentCommand;
+import com.appsdeveloperblog.core.exceptions.CreditCardProcessorUnavailableException;
+import com.appsdeveloperblog.payments.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics = {"${payments.commands.topic.name}"})
+public class PaymentsCommandsHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PaymentsCommandsHandler.class);
+
+    private final PaymentService paymentService;
+
+    public PaymentsCommandsHandler(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+
+    @KafkaHandler
+    public void handleCommand(@Payload ProcessPaymentCommand command) {
+        try {
+            Payment payment = new Payment(
+                    command.getOrderId(),
+                    command.getProductId(),
+                    command.getProductPrice(),
+                    command.getProductQuantity()
+            );
+            paymentService.process(payment);
+        } catch (CreditCardProcessorUnavailableException e) {
+            LOGGER.error(e.getLocalizedMessage(), e);
+        }
+    }
+}
